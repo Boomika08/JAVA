a52d556e2c89728b0cf1d452af7cbf6b16900f5e
BOOMIKA.P <74901037+Boomika08@users.noreply.github.com>
2024-08-16 21:52:23 +0530

    Add files via upload

---
+++ task7/unboxing.java
@@ -0,0 +1,17 @@
+package task;
+
+public class unboxing {
+
+	public static void main(String[] args) {
+		Integer i=30;//class type Integer
+		int a=i;//converting class type to primitive data type implicitly by the compiler
+		System.out.println("Primitive datatype int: "+a);
+		
+		Character c='h';//class type Character
+		char b=c;//converting class type to primitive data type implicitly by the compiler
+		System.out.println("Primitive datatype int: "+b);
+	}
+
+}
+
+ 
